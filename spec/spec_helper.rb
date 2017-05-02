8f3e3406aeeb4406e89daebaff3c78fffff21303
Paula <pmuldoon@umich.edu>
2017-05-02 16:54:34 +0100

    added index.erb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,16 @@
 require './app/models/link'
+
+
+
+ENV['RACK_ENV'] = 'test'
+
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', './app/app.rb')
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+Capybara.app = BookmarkManager
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -9,28 +18,16 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
   config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
+
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-  # rspec-mocks config goes here. You can use an alternate test double
-  # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
+
     mocks.verify_partial_doubles = true
   end
 
