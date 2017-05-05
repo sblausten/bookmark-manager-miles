0272e05d16e91962d7595458eca89a3660f61b6f
milesillsley <milesillsley@hotmail.co.uk>
2017-05-05 12:35:03 +0100

    Passing multiple tag tests

--- app/app.rb
+++ app/app.rb
@@ -17,8 +17,8 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tag])
-    link.tags << tag
+    tags = params[:tag].split(', ')
+    tags.each { |tag| link.tags << Tag.first_or_create(name: tag)}
     link.save
     redirect('/links')
   end
